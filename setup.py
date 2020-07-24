f4c751143fda80aedc18e25e73bec6f5bc4e69f1
Oshgnacknak <osh@oshgnacknak.de>
2020-07-24 18:08:56 +0200

    Initial setup.py and .gitignore

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description=f.read()
+
+setup(
+    name='trf',
+    version='0.0.1',
+    author='Schachklub Langen e. V.',
+    author_email='Turnierleiter@sklangen.de',
+    description='A parser and dumper for the fide approved tournament report format: trf',
+    license='GPLv3+',
+    keywords=['trf', 'fide', 'chess', 'tournaments'],
+    packages=['trf'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Utilities',
+    ],
+)
